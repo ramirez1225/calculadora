30a5b7c9540a08b08b65c5de3c65e3c8cd089907
mac14 <19170169@uttcampus.edu.mx>
2020-10-20 17:19:21 -0500

    agregue splash con temporizador

--- app/src/main/java/com/example/primerproyecto/splash.java
+++ app/src/main/java/com/example/primerproyecto/splash.java
@@ -3,20 +3,38 @@ package com.example.primerproyecto;
 import android.app.Activity;
 import android.content.Intent;
 import android.os.Bundle;
+import android.os.CountDownTimer;
 import android.view.View;
+import android.widget.TextView;
 
-public class splash extends Activity implements View.OnClickListener {
-
+public class splash extends Activity{
+    TextView e;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_splash);
-        findViewById(R.id.btr).setOnClickListener(this);
+
+        e =findViewById(R.id.txt);
+
+        new CountDownTimer(5000, 1000) {
+            @Override
+            public void onTick(long l) {
+                long t=  (l/1000);
+                String str=String.valueOf(t);
+                e.setText(str);
+            }
+
+            @Override
+            public void onFinish() {
+                startActivity(new Intent(getApplicationContext(),MainActivity.class));
+                finish();
+
+            }
+
+        }.start();
+
     }
 
-    @Override
-    public void onClick(View v) {
-        Intent ir=new Intent(this,MainActivity.class);
-        startActivity(ir);
+
     }
-}
+
